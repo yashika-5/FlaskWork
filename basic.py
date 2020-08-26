220f4bdd557d64d007b23a90c8f39fde551bde79
yashika-5 <yashikagupta2021@gmail.com>
2020-08-26 16:55:38 +0530

    Template variable and Control statement

--- basic.py
+++ basic.py
@@ -1,19 +1,26 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/') # 127.0.0.1:5000
 def index():
-    return "<h1>Hello World</h1>"
+    return render_template('basic.html')
 
-@app.route('/info') # 127.0.0.1:5000/info
+
+@app.route('/info')  # 127.0.0.1:5000/info
 def info():
-    return "<h1>Information page</h1>"
+    name = 'yashika'
+    letters = list(name)
+    mylist = ['rufus','fluffy','spiky']
+    mydictionary = {'name': 'Jose'}
+    some_variable = "Dude"
+    return render_template('tmp_basic.html', my_var=some_variable, letters=letters, mydictionary=mydictionary, mylist=mylist)
 
 
 @app.route('/users_data/<name>')
 def user(name):
     return f"<h1>This is a page for {name.upper()}</h1>"
 
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
