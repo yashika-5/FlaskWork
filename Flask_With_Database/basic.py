6491ede4b7d500fe91b50c9fbfee9a79756e4af7
yashika-5 <yashikagupta2021@gmail.com>
2020-08-31 23:23:06 +0530

    Create table using SQLite

---
+++ Flask_With_Database/basic.py
@@ -0,0 +1,35 @@
+import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+# print(basedir)
+# print(__file__) -- basic.py
+app = Flask(__name__)
+
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class User(db.Model):
+
+    __tablename__ = 'student'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.Text)
+    age = db.Column(db.Integer)
+    dob = db.Column(db.DateTime)
+
+    def __init__(self,name,age,dob):
+        self.name = name
+        self.age = age
+        self.dob = dob
+
+    def __repr__(self):
+        return f"Student Name :{self.name}\n Age :{self.age}\n DOB :{self.dob}"
+
+
+
